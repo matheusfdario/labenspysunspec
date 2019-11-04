4d340134cd0460c3e9d09ce713c50adcd28965a8
matheusfdario <30605849+matheusfdario@users.noreply.github.com>
2019-11-04 16:58:28 -0300

    upgrade ip

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import sunspec.core.client as client
 
-data = client.SunSpecClientDevice(client.TCP, 1, ipaddr="10.80.15.78", ipport=502, timeout=10)
+data = client.SunSpecClientDevice(client.TCP, 1, ipaddr="10.80.10.158", ipport=502, timeout=10)
 
 data.inverter.read()
 
